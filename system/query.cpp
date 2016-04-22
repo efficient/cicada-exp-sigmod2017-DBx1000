34e1cc99c09347ac3dd47f8b180affa231b83a97
Hyeontaek Lim <hl@cs.cmu.edu>
2016-04-22 02:42:14 -0400

    Fix out of range when abort buffer is enabled

--- system/query.cpp
+++ system/query.cpp
@@ -73,7 +73,7 @@ Query_thd::init(workload * h_wl, int thread_id) {
 	uint64_t request_cnt;
 	q_idx = 0;
 	// request_cnt = WARMUP / g_thread_cnt + MAX_TXN_PER_PART + 4;
-	request_cnt = WARMUP + MAX_TXN_PER_PART + 4;
+	request_cnt = WARMUP + MAX_TXN_PER_PART + ABORT_BUFFER_SIZE * 2;
 #if WORKLOAD == YCSB
 	queries = (ycsb_query *)
 		mem_allocator.alloc(sizeof(ycsb_query) * request_cnt, thread_id);
