88896159a4a380e4a5d69c8d5e310a1840e6474a
Hyeontaek Lim <hl@cs.cmu.edu>
2016-08-20 00:40:16 -0400

    Abort TX on getCId() failure

--- benchmarks/tpcc_txn.cpp
+++ benchmarks/tpcc_txn.cpp
@@ -1070,11 +1070,10 @@ RC tpcc_txn_man::run_delivery(tpcc_query* query) {
 
     auto order = delivery_getCId(o_id, d_id, arg.w_id);
     if (order == NULL) {
-      // There is no guarantee that we will see a order row even after seeing a related new_order row in this read-write transaction.  We ignore this (as in Silo) and try later.
-      continue;
+      // There is no guarantee that we will see a order row even after seeing a related new_order row in this read-write transaction.
       // FAIL_ON_ABORT();
       // printf("oops1\n");
-      // return finish(Abort);
+      return finish(Abort);
     }
     uint64_t c_id;
     order->get_value(O_C_ID, c_id);
