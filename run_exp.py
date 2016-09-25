eeaf7005aa962b3d6d18382de5fca2008b042922
Hyeontaek Lim <hl@cs.cmu.edu>
2016-09-24 20:51:25 -0400

    Enable all YCSB exps for 2PL and Hekaton

--- run_exp.py
+++ run_exp.py
@@ -305,9 +305,9 @@ def enum_exps(seq):
           for read_ratio in [0.50, 0.95]:
             # for zipf_theta in [0.00, 0.90, 0.99]:
             for zipf_theta in [0.00, 0.99]:
-              if zipf_theta >= 0.95:
-                if read_ratio == 0.50 and alg == 'NO_WAIT': continue
-                if read_ratio == 0.50 and alg == 'HEKATON': continue
+              #if zipf_theta >= 0.95:
+              #  if read_ratio == 0.50 and alg == 'NO_WAIT': continue
+              #  if read_ratio == 0.50 and alg == 'HEKATON': continue
               ycsb.update({ 'read_ratio': read_ratio, 'zipf_theta': zipf_theta })
               yield dict(ycsb)
 
@@ -399,9 +399,9 @@ def enum_exps(seq):
 
           for read_ratio in [0.50, 0.95]:
             for zipf_theta in [0.00, 0.40, 0.60, 0.80, 0.90, 0.95, 0.99]:
-              if zipf_theta >= 0.95:
-                if read_ratio == 0.50 and alg == 'NO_WAIT': continue
-                if read_ratio == 0.50 and alg == 'HEKATON': continue
+              #if zipf_theta >= 0.95:
+              #  if read_ratio == 0.50 and alg == 'NO_WAIT': continue
+              #  if read_ratio == 0.50 and alg == 'HEKATON': continue
               ycsb.update({ 'read_ratio': read_ratio, 'zipf_theta': zipf_theta })
               yield dict(ycsb)
 
